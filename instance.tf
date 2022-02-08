53b9b705076e955ca9b4c3b908a64a174e5d8dd0
Collins Muriuki <hello@collinsmuriuki.xyz>
2022-02-08 05:06:42 +0300

    Add ssh key resource

--- instance.tf
+++ instance.tf
@@ -1,6 +1,12 @@
 resource "vultr_instance" "collins" {
-	plan = var.vultr_instance_plan
-	region = var.vultr_instance_region
-	os_id = var.vultr_os_id
-	hostname = "collins"
+  plan        = var.vultr_instance_plan
+  region      = var.vultr_instance_region
+  os_id       = var.vultr_os_id
+  hostname    = "collins"
+  ssh_key_ids = [vultr_ssh_key.collinsmuriuki.id]
+}
+
+resource "vultr_ssh_key" "collinsmuriuki" {
+  name    = "collinsmuriuki"
+  ssh_key = var.vultr_ssh_key
 }
--- variables.tf
+++ variables.tf
@@ -9,23 +9,28 @@ variable "vultr_retry_limit" {
 }
 
 variable "vultr_api_key" {
-  default = ""
-	sensitive = true
-  type    = string
+  default   = ""
+  sensitive = true
+  type      = string
 }
 
 variable "vultr_instance_plan" {
-	default = "vc2-1c-1gb"
-	type = string
+  default = "vc2-1c-1gb"
+  type    = string
 }
 
 variable "vultr_instance_region" {
-	default = "fra"
-	type = string
+  default = "fra"
+  type    = string
 }
 
 variable "vultr_os_id" {
-	default = 352
-	desdescription = "Debian 10 x64 (buster)"
-	type = number
+  default     = 352
+  description = "Debian 10 x64 (buster)"
+  type        = number
+}
+
+variable "vultr_ssh_key" {
+  default = ""
+  type    = string
 }
